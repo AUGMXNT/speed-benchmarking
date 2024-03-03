eae7f73d10b7a8496208d0b6e9d1176d1572f92a
lhl <lhl@randomfoo.net>
2024-03-04 07:12:57 +0900

    updated

--- speed-benchmark/setup.sh
+++ speed-benchmark/setup.sh
@@ -1,18 +1,26 @@
+# System
+apt install htop inxi nvtop
+apt install neovim
+
 # Install Mamba
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3-$(uname)-$(uname -m).sh
 
 # Setup
-~/miniforge/bin/mamba init
+~/miniforge3/bin/mamba init
 source ~/.bashrc
 
 # Install requirements
-# mamba install -c "nvidia/label/cuda-12.1.1" cuda-toolkit -y
-mamba install -c "nvidia/label/cuda-11.8.0" cuda-toolkit -y
-mamba install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+mamba install -c "nvidia/label/cuda-12.1.1" cuda-toolkit -y
+mamba install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+# mamba install -c "nvidia/label/cuda-11.8.0" cuda-toolkit -y
+# mamba install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+mamba update ffmpeg
 
-pip install transformers dataset
+pip install transformers dataset accelerate
 pip install loguru
 pip install git+https://github.com/m-bain/whisperx.git
 pip install faster-whisper -U
 pip install librosa soundfile -U
+
+export LD_LIBRARY_PATH=/root/miniforge3/lib/python3.10/site-packages/torch/lib
