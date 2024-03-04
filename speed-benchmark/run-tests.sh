5924e15e182f3fd2461668c942c0c7af59aa37d6
lhl <lhl@randomfoo.net>
2024-03-04 19:47:03 +0000

    update libs, better output

--- speed-benchmark/run-tests.sh
+++ speed-benchmark/run-tests.sh
@@ -1,5 +1,12 @@
-export LD_LIBRARY_PATH=/workspace/miniforge3/lib/python3.10/site-packages/torch/lib
+export LD_LIBRARY_PATH=/workspace/miniforge3/lib/python3.10/site-packages/torch/lib:/workspace/miniforge3/lib/python3.10/site-packages/nvidia/cudnn/lib
 
+echo 'test-hf-pipe.py'
+echo '---'
 for i in {1..5}; do python test-hf-pipe.py | grep DEBUG; sleep 1; done
+echo
+echo 'test-whisperx.py'
+echo '---'
 for i in {1..5}; do python test-whisperx.py | grep DEBUG; sleep 1; done
+echo 'test-fasterwhisper.py'
+echo '---'
 for i in {1..5}; do python test-fasterwhisper.py | grep DEBUG; sleep 1; done
