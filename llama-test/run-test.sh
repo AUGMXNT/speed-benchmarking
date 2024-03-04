563e9dde72df20e461b899f327a85269f24ab377
lhl <lhl@randomfoo.net>
2024-03-04 19:36:59 +0000

    updates for compiling with vast.ai images

--- llama-test/run-test.sh
+++ llama-test/run-test.sh
@@ -1,2 +1,2 @@
-llama.cpp/llama-bench -p 3968 -m llama-2-7b.Q4_0.gguf
-llama.cpp/llama-bench -p 3968 -m llama-2-7b.Q4_K_M.gguf
+llama.cpp/build/bin/llama-bench -p 3968 -m llama-2-7b.Q4_0.gguf
+llama.cpp/build/bin/llama-bench -p 3968 -m llama-2-7b.Q4_K_M.gguf
--- llama-test/setup.sh
+++ llama-test/setup.sh
@@ -1,6 +1,14 @@
 # vast images require this
 apt install -y build-essential
 
+# vast images have an LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/workspace/miniforge3/lib
+export MAKEFLAGS="-j$(nproc)"
+
 git clone https://github.com/ggerganov/llama.cpp
 cd llama.cpp
-make -j$(nproc) LLAMA_CUBLAS=1
+mkdir build
+cd build
+cmake .. -DLLAMA_CUBLAS=on
+cmake --build . --config Release
+# make -j$(nproc) LLAMA_CUBLAS=1
